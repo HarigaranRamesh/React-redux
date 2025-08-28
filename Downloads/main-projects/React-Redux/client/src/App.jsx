7bd428e7f22c45e8055f36ab7a1c0e9799910fbf
Harigaran Ramesh <deepakkannan@Harigarans-Macbook.local>
2025-08-28 23:25:59 +0530

    Fix: removed submodule, added client as normal folder

---
+++ Downloads/main-projects/React-Redux/client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./Store";
+import EmployeeList from "./EmployeeList";
+import EmployeeForm from "./EmployeeForm";
+import "./Styles.css";
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="card">
+        <EmployeeForm />
+        <EmployeeList />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
